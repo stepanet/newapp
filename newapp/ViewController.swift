ab8843309c39ddde2722977524bb6a444a4e0bb0
Денис Сидоренко <itklass@mail.ru>
2019-02-25 21:26:44 +0300

    Initial Commit

---
+++ newapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newapp
+//
+//  Created by Денис Сидоренко on 25/02/2019.
+//  Copyright © 2019 Денис Сидоренко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
